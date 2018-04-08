f491466808b67db15d2525764af9e95a791df25b
xingjing Lu <xingjinglu@gmail.com>
2018-04-09 00:11:34 +0800

    up

--- LearningTensorflow/ch3.4.3.py
+++ LearningTensorflow/ch3.4.3.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 w1 = tf.Variable(tf.random_normal((2, 3), stddev=1, seed=1))
 w2 = tf.Variable(tf.random_normal((3, 1), stddev=1, seed=1))
 
-x = tf.constant([0.7, 0.9]])
+x = tf.constant([[0.7, 0.9]])
 
 a = tf.matmul(x, w1)
-y = tf.matmul(a. w2)
+y = tf.matmul(a, w2)
 
 sess = tf.Session()
 
