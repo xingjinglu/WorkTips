8cd4ca2a6ee5012181471b199070d3da2ab77987
xingjinglu <ericlew1840@gmail.com>
2018-04-09 00:01:21 +0800

    Create ch3.4.3.py

---
+++ LearningTensorflow/ch3.4.3.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+w1 = tf.Variable(tf.random_normal((2, 3), stddev=1, seed=1))
+w2 = tf.Variable(tf.random_normal((3, 1), stddev=1, seed=1))
+
+x = tf.constant([0.7, 0.9]])
+
+a = tf.matmul(x, w1)
+y = tf.matmul(a. w2)
+
+sess = tf.Session()
+
+sess.run(w1.initializer)
+sess.run(w2.initializer)
+
+print(sess.run(y))
+sess.close()
