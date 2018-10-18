66fb25eef3cf2f7605958dd503e920ef854f4110
Xingjing Lu <luxingjing@sogou-inc.com>
2018-10-18 14:53:39 +0800

    up vim/install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -10,6 +10,8 @@ unzip master.zip
 cd vim-master/src/
 ./configure && make -j64 && make install
 
+rm vim-master master.zip -rf
+
 sudo mv /usr/bin/vim /usr/bin/vim.bak
 sudo ln /usr/local/bin/vim /usr/bin -s
 
@@ -18,33 +20,25 @@ sudo ln /usr/local/bin/vim /usr/bin -s
 # ~/.vim/bundle是pathogen默认runtimepath，把所有的plugin放到该目录即可
 mkdir -p ~/.vim/autoload ~/.vim/bundle  && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-cat > ~/.vimrc <<EOF
-execute pathogen#infect()
-syntax on
-filetype plugin indent on
-EOF
+
 
 
 
 # Install vim-plugin  
 
-cd ~/.vim/bundle
-git clone https://github.com/majutsushi/tagbar.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/powerline/powerline.git
-git clone https://github.com/fatih/vim-go.git
-git clone https://github.com/tpope/vim-sensible.git
-git clone https://github.com/Shougo/neocomplete.vim.git
-git clone https://github.com/sjl/gundo.vim
-git clone https://github.com/Blackrush/vim-gocode.git
-git clone https://github.com/plasticboy/vim-markdown.git
+git clone https://github.com/majutsushi/tagbar.git   ~/.vim/bundle/tagbar
+git clone https://github.com/scrooloose/nerdtree.git  ~/.vim/bundle/nerdtree
+git clone https://github.com/powerline/powerline.git ~/.vim/bundle/powerline
+git clone https://github.com/fatih/vim-go.git  ~/.vim/bundle/vim-go
+git clone https://github.com/tpope/vim-sensible.git ~/.vim/vim-sensible
+git clone https://github.com/Shougo/neocomplete.vim.git ~/.vim/neocomplete.vim
+git clone https://github.com/sjl/gundo.vim  ~/.vim/gundo.vim
+git clone https://github.com/Blackrush/vim-gocode.git  ~/.vim/vim-gocode
+git clone https://github.com/plasticboy/vim-markdown.git ~/.vim/vim-markdown
 
-# Generate help docs
-#vim t
-#:Helptags  # 自动生成所有plugin的文档
 
-cat >> ~/.bashrc <<EOF
 mkdir -p  ~/software/go_workspace
+cat >> ~/.bashrc <<EOF
 export GOPATH=~/software/go_workspace
 export GOROOT=/usr/lib/golang # 默认安装目录
 export PATH=$PATH:$GOPATH/bin
@@ -63,7 +57,69 @@ go get -u github.com/mdempsky/gocode
 #vim t
 #::GoInstallBinaries
 
-cat >> ~/.vimrc <<EOF
+mv ~/.vimrc ~/.vimrc.bak
+cat >  ~/.vimrc <<EOF
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+
+" cscope related
+set nocscopeverbose  
+
+set mouse=ar
+set hlsearch
+colorscheme desert
+set smartindent
+set expandtab
+set shiftwidth=2
+set tabstop=2
+set autoindent
+
+syntax on
+set hlsearch
+set showmatch
+
+
+
+let Tlist_Auto_Open = 0
+let Tlist_Auto_Update = 0
+
+set cscopetag
+set textwidth=80
+
+
+set nocompatible
+set t_Co=256
+
+set wildmenu
+set wildmode=longest:list,full
+
+set completeopt=longest,menu,preview
+set backspace=indent,eol,start
+
+set history=200
+
+set laststatus=2
+set showcmd
+set incsearch
+set splitright
+set splitbelow
+
+au FileType python set expandtab shiftwidth=4 tabstop=4
+
+" OpenCL format.
+autocmd BufNewFile,BufRead *.cl set filetype=opencl
+autocmd FileType opencl source /search/speech/luxingjing/.vim/plugin/opencl.vim
+
+"NerdTree
+nnoremap <silent> <F2> :NERDTree <CR>
+
+"TagbarToggle
+nnoremap <silent> <F7> :TagbarToggle<CR>
+let TagbarOpenAutoClose = 0
+let tagbar_autoclose = 0
+let tagbar_autoopen = 1
+
 "golang                                                                                                                                             
 let g:tagbar_type_go = {                                                                                                                            
   \ 'ctagstype' : 'go',
@@ -96,5 +152,14 @@ EOF
 
 
 
+# Install other stuffs.
 
+
+# Generate help docs
+#vim t
+#:Helptags  # 自动生成所有plugin的文档
+
+pushd ~/.vim/bundle
+echo "vim t"
+echo ":GoInstallBinaries"
 
