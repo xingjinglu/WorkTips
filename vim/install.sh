d20ea33d744cdadc162f6e2bd2dd59e906c7f47b
Xingjing Lu <xingjinglu@gmail.com>
2018-10-18 14:54:37 +0800

    up

--- vim/install.sh
+++ vim/install.sh
@@ -10,6 +10,8 @@ unzip master.zip
 cd vim-master/src/
 ./configure && make -j64 && make install
 
+rm vim-master master.zip -rf
+
 sudo mv /usr/bin/vim /usr/bin/vim.bak
 sudo ln /usr/local/bin/vim /usr/bin -s
 
@@ -18,10 +20,69 @@ sudo ln /usr/local/bin/vim /usr/bin -s
 # ~/.vim/bundle是pathogen默认runtimepath，把所有的plugin放到该目录即可
 mkdir -p ~/.vim/autoload ~/.vim/bundle  && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-cat > ~/.vimrc <<EOF
+mv ~/.vimrc ~/.vimrc.bak
+cat >  ~/.vimrc <<EOF
 execute pathogen#infect()
 syntax on
 filetype plugin indent on
+
+" cscope related
+set nocscopeverbose  
+
+set mouse=ar
+set hlsearch
+colorscheme desert
+set smartindent
+set expandtab
+set shiftwidth=2
+set tabstop=2
+set autoindent
+
+syntax on
+set hlsearch
+set showmatch
+
+
+
+let Tlist_Auto_Open = 0
+let Tlist_Auto_Update = 0
+
+set cscopetag
+set textwidth=80
+
+
+set nocompatible
+set t_Co=256
+
+set wildmenu
+set wildmode=longest:list,full
+
+set completeopt=longest,menu,preview
+set backspace=indent,eol,start
+
+set history=200
+
+set laststatus=2
+set showcmd
+set incsearch
+set splitright
+set splitbelow
+
+au FileType python set expandtab shiftwidth=4 tabstop=4
+
+" OpenCL format.
+autocmd BufNewFile,BufRead *.cl set filetype=opencl
+autocmd FileType opencl source /search/speech/luxingjing/.vim/plugin/opencl.vim
+
+"NerdTree
+nnoremap <silent> <F2> :NERDTree <CR>
+
+"TagbarToggle
+nnoremap <silent> <F7> :TagbarToggle<CR>
+let TagbarOpenAutoClose = 0
+let tagbar_autoclose = 0
+let tagbar_autoopen = 1
+
 EOF
 
 
@@ -96,5 +157,8 @@ EOF
 
 
 
-
+# Install other stuffs.
+pushd ~/.vim/bundle
+echo "vim t"
+echo ":GoInstallBinaries"
 
