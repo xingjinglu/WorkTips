25503f3d002896c0b81c3b7632d803a7d2022ede
Xingjing Lu <luxigjing@sogou-inc.com>
2021-06-01 17:00:49 +0800

    up

---
+++ ctypes/test2.py
@@ -0,0 +1,10 @@
+from ctypes import *
+
+class Bottles:
+    def __init__(self, number):
+        self._as_parameter_ = number
+
+
+bottles = Bottles(42)
+print(bottles)
+print(b"%d bottles of beer\n", bottles)
